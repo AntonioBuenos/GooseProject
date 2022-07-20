4fdffe3fcbc34518746ecc1349f8a52e58a737bf
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-07-20 09:25:19 +0300

    T1 amended more

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hoorray!");
+
+        System.out.println("Task 1 done");
+
+        System.out.println("Task 1 done ammended");
+        System.out.println("Task 1 done ammended twice");
     }
 }
